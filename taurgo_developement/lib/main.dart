ba43c111e11fbd1793355555875af15af518d57c
sandunil <sandunil.rajapaksha@taurgo.co.uk>
2024-08-12 21:10:40 +0530

    Final changes for Version 1.0

--- taurgo_developement/lib/main.dart
+++ taurgo_developement/lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Taurgo',
       theme: ThemeData(
         scaffoldBackgroundColor: bWhite,
